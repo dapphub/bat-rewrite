e569ff0856167b05aa6b52348c4c0acdad9d276a
Nikolai Mushegian <nikolai@nexusdev.us>
2017-04-22 11:28:31 -0400

    add example custodian bonus logic

--- src/bat.sol
+++ src/bat.sol
@@ -5,18 +5,18 @@ contract BATCrowdsale {
     DSToken BAT;
 
     address custodian;
-    uint custodianBonus;
-    uint saleStart;
-    uint saleEnd;
+    uint custodianRatio; // as a whole number of percent, e.g. 30 -> 30%
     uint limit;
     uint minimum;
+    uint saleStart;
+    uint saleEnd;
     bool done; // set true on finalize
     bool ok;   // set true on finalize only if all conditions met
 
     function BATCrowdsale(address custodian_, uint saleStart_, uint saleEnd_)
     {
         BAT = new DSToken();
-        BAT.stop(); // Don't allow ERC20 actions during the sale.
+        BAT.stop(); // Don't allow ERC20 actions until/unless sale succeeds
         custodian = custodian_;
         saleStart = saleStart_;
         saleEnd = saleEnd_;
@@ -34,11 +34,10 @@ contract BATCrowdsale {
         assert( saleEnd < now );
         var supply = BAT.totalSupply();
         done = true;
-        // limit check is implicit via `buy`
         ok = (minimum <= supply) && (supply <= limit);
         if( ok ) {
-            // TODO compute ratio
-            BAT.mint(custodian, custodianBonus);
+            var reward = supply * custodianRatio / 100;
+            BAT.mint(custodian, reward);
             BAT.flow(); // re-enable ERC20 actions
             BAT.setOwner(msg.sender);
         }
@@ -51,3 +50,11 @@ contract BATCrowdsale {
         msg.sender.call.value(bal)();
     }
 }
+
+// For easier instantiation from multisig
+contract BATCrowdsaleFactory {
+    function make(address c, uint s, uint e) returns (BATCrowdsale) {
+        return new BATCrowdsale(c, s, e);
+    }
+}
+
